1215dcad25654862af8fd8d3e7b2f7606c4a5370
Pritam Dhar <pritamdhar6568@gmail.com>
2024-09-27 11:51:07 +0530

    minor chnages

--- src/main.jsx
+++ src/main.jsx
@@ -14,37 +14,37 @@ import {
   useNavigationType,
 } from "react-router-dom";
 
-Sentry.init({
-  dsn: "https://4386472deb0d04d2e19768984c58d156@o4507967709380608.ingest.de.sentry.io/4507967785926736",
-  integrations: [
-    // See docs for support of different versions of variation of react router
-    // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
-    Sentry.reactRouterV6BrowserTracingIntegration({
-      useEffect,
-      useLocation,
-      useNavigationType,
-      createRoutesFromChildren,
-      matchRoutes,
-    }),
-    Sentry.replayIntegration({
-      // Additional SDK configuration goes in here, for example:
-      maskAllText: true,
-      blockAllMedia: true,
-    }),
-  ],
+// Sentry.init({
+//   dsn: "https://4386472deb0d04d2e19768984c58d156@o4507967709380608.ingest.de.sentry.io/4507967785926736",
+//   integrations: [
+//     // See docs for support of different versions of variation of react router
+//     // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
+//     Sentry.reactRouterV6BrowserTracingIntegration({
+//       useEffect,
+//       useLocation,
+//       useNavigationType,
+//       createRoutesFromChildren,
+//       matchRoutes,
+//     }),
+//     Sentry.replayIntegration({
+//       // Additional SDK configuration goes in here, for example:
+//       maskAllText: true,
+//       blockAllMedia: true,
+//     }),
+//   ],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for tracing.
-  tracesSampleRate: 1.0,
+//   // Set tracesSampleRate to 1.0 to capture 100%
+//   // of transactions for tracing.
+//   tracesSampleRate: 1.0,
 
-  // Set tracePropagationTargets to control for which URLs trace propagation should be enabled
-  tracePropagationTargets: [/^\//, /^https:\/\/yourserver\.io\/api/],
+//   // Set tracePropagationTargets to control for which URLs trace propagation should be enabled
+//   tracePropagationTargets: [/^\//, /^https:\/\/yourserver\.io\/api/],
 
-  // Capture Replay for 10% of all sessions,
-  // plus for 100% of sessions with an error
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
+//   // Capture Replay for 10% of all sessions,
+//   // plus for 100% of sessions with an error
+//   replaysSessionSampleRate: 0.1,
+//   replaysOnErrorSampleRate: 1.0,
+// });
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
